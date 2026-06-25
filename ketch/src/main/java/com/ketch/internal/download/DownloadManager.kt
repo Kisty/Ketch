f2d8f3e2dbafccf240f08177b0775e13bbeeb9ae
Tim Kist <timkist@imagitech.co.uk>
2026-06-25 17:29:04 +0100

    Don't replace downloads, instead reuse the current ones

--- ketch/src/main/java/com/ketch/internal/download/DownloadManager.kt
+++ ketch/src/main/java/com/ketch/internal/download/DownloadManager.kt
@@ -254,7 +254,7 @@ internal class DownloadManager(
 
         workManager.enqueueUniqueWork(
             downloadRequest.id.toString(),
-            ExistingWorkPolicy.REPLACE,
+            ExistingWorkPolicy.KEEP,
             downloadWorkRequest
         )
     }
